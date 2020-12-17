3f0f01b63dc41fa74743dd7242737ede0d98c6cc
wseis <wolfgang.seis@kompetenz-wasser.de>
2020-12-17 13:32:41 +0100

    flexdash

--- hydrology_dev.R
+++ hydrology_dev.R
@@ -44,10 +44,29 @@ get_q_zulaessig <- function(Hq1_pnat= get_Hq1_pnat(), x = get_x(), A_ba, A_E0){
 # Calculate relevant discharge to surface water (rainwater only)
 
 
-get_q_area_partial <- function(A_ba, f_DA, R_spende=10){
+get_q_area_partial <- function(A_ba, f_DA, R_spende=1){
   A_ba*f_DA*R_spende
 }
 
+
+# calculate relevant discharge of table of areas
+get_q_area_total <- function(dataframe, R_Spende=1){
+  
+  mean(dataframe$f_DA)*sum(dataframe$A_ba)*R_Spende
+  
+}
+
+# get average permeability
+
+get_average_runoff_coef <- function(A_ba, q_zul, R_Spende){
+  q_zul/(R_Spende*A_ba)
+}
+
+# get average permeability
+
+get_allowed_area <- function(f_DA=0.7, q_zul, R_Spende=150){
+  q_zul/(R_Spende*f_DA)
+}
 
 
 
