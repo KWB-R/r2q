fb9761d0088a1bd8bca126a38fcb72be4e4d54b9
wseis <wolfgang.seis@kompetenz-wasser.de>
2021-05-06 10:58:32 +0200

    refactoring .apply_hydrology

--- R/.apply_hydrology.R
+++ R/.apply_hydrology.R
@@ -1,37 +1,38 @@
 library(magrittr)
 #---------import data -----------------------------
 
-hydrology <- r2q::import_hydrology_data()
+
+KOSTRA_string_from_numeric <- function(time_numeric=hydrology$tf){
+  
+  # constructing number of zeros for duration string
+  length <- stringr::str_count(time_numeric)
+  zeros <- paste(replicate(4 - length , "0"), collapse = "")
+  paste0(zeros, time_numeric)
+}
 
 
-calculate_tolerable_discharge(hydrology = hydrology)
+calculate_relevant_discharge <- function(data =r2q::import_hydrology_data(), 
+                                         area_classification = T,
+                                         path_to_area="./inst/extdata/areas_hydrology.csv"){
 
-
-
-planning_area <- readr::read_csv(paste0(data.dir,"areas_hydrology.csv"))
-
-
-calculate_relevant_discharge <- function(data = hydrology, area_classification = F){
-
+      # loading area data
+      #planning_area <- readr::read_csv(path_to_area)#paste0(data.dir,"areas_hydrology.csv"))
   
     # reshape for easier handling
-    hydrology <- hydrology %>% 
+    hydrology <- data %>% 
       dplyr::select(Abkuerzung.A102, Wert) %>% # select relevant columns
       tidyr::spread(Abkuerzung.A102, Wert) # spread dataframe for "$" referencing
     
     
-    KOSTRA_string_from_numeric <- function(length = stringr::str_count(hydrology$tf)){
-      # constructing number of zeros for duration string
-      zeros <- paste(replicate(4 -length , "0"), collapse = "")
-      zeros
-    }
-    
-  
+    duration_string <- KOSTRA_string_from_numeric(hydrology$tf)
     # get KOSTRA rainfall based on coordinates and 
-    rainfall <- r2q::get_KOSTRA(duration_string = paste0(KOSTRA_string_from_numeric(), 
-                                                       hydrology$tf), plot = T)
-  
+    rainfall <- r2q::get_KOSTRA(duration_string = duration_string,  plot = T)
+    
     if(area_classification){
+      # loading area data
+      planning_area <- readr::read_csv(path_to_area)#paste0(data.dir,"areas_hydrology.csv"))
+      
+     
       # check for consistencies
       if(!planning_area %>% dplyr::filter(Einheit =="ha") %>% 
       dplyr::summarise(sum = sum(Wert)/100) == hydrology$A_plan){
@@ -40,9 +41,7 @@ calculate_relevant_discharge <- function(data = hydrology, area_classification =
       }
    }
 
-
-
-#rainfall <- r2q::get_KOSTRA(duration_string = "0010", plot = T)
+    #rainfall <- r2q::get_KOSTRA(duration_string = "0010", plot = T)
     result <- list()
     
     for(i in unique(planning_area$Type)){
@@ -70,8 +69,17 @@ calculate_relevant_discharge <- function(data = hydrology, area_classification =
     
     
     QE1_total <- sum(result$qe_partial)
+    QE1_total
+  }
     
-    ratio_QE <- QE1_total / Q_tolerable_planning
+    
+assess_discharges <- function(){    
+     
+     QE1_total <- calculate_relevant_discharge()
+     Q_tolerable_planning <- r2q::calculate_tolerable_discharge(hydrology = r2q::import_hydrology_data(),
+                                                               verbose = F)
+    
+     ratio_QE <- QE1_total / Q_tolerable_planning
     
     if(QE1_total > Q_tolerable_planning){
       print(paste("Discharges exceed tolerable levels by", 
@@ -85,7 +93,6 @@ calculate_relevant_discharge <- function(data = hydrology, area_classification =
     }
 
 }
-
 
 
 
--- R/hydrology.R
+++ R/hydrology.R
@@ -231,3 +231,133 @@ calculate_tolerable_discharge <- function(hydrology = import_hydrology_data(),
 
 
 
+
+
+
+#' KOSTRA_string_from_numeric
+#'
+#' @param time_numeric numeric value inline with KOSTRA duration
+#'
+#' @return returns string suitable for input in get KOSTRA function
+#' @export
+#'
+#' @examples KOSTRA_string_from_numeric(10)
+#' KOSTRA_string_from_numeric(360)
+#' 
+KOSTRA_string_from_numeric <- function(time_numeric=hydrology$tf){
+  
+  # constructing number of zeros for duration string
+  length <- stringr::str_count(time_numeric)
+  zeros <- paste(replicate(4 - length , "0"), collapse = "")
+  paste0(zeros, time_numeric)
+}
+
+
+#' calculate_relevant_discharge 
+#'
+#' @param data hydrology (tibble as retrieved by \code{\link{import_hydrology_data}})
+#' @param area_classification if TRUE perfomes area specific evaluation based on table
+#' @param path_to_area path the csv file containin garea information
+#'
+#' @return returns relevant discharge from planning area
+#' @export
+#'
+#' @examples
+#' 
+calculate_relevant_discharge <- function(data =r2q::import_hydrology_data(), 
+                                         area_classification = T,
+                                         path_to_area="./inst/extdata/areas_hydrology.csv"){
+  
+  # loading area data
+  #planning_area <- readr::read_csv(path_to_area)#paste0(data.dir,"areas_hydrology.csv"))
+  
+  # reshape for easier handling
+  hydrology <- data %>% 
+    dplyr::select(Abkuerzung.A102, Wert) %>% # select relevant columns
+    tidyr::spread(Abkuerzung.A102, Wert) # spread dataframe for "$" referencing
+  
+  
+  duration_string <- KOSTRA_string_from_numeric(hydrology$tf)
+  # get KOSTRA rainfall based on coordinates and 
+  rainfall <- r2q::get_KOSTRA(duration_string = duration_string,  plot = T)
+  
+  if(area_classification){
+    # loading area data
+    planning_area <- readr::read_csv(path_to_area)#paste0(data.dir,"areas_hydrology.csv"))
+    
+    
+    # check for consistencies
+    if(!planning_area %>% dplyr::filter(Einheit =="ha") %>% 
+       dplyr::summarise(sum = sum(Wert)/100) == hydrology$A_plan){
+      
+      stop("Error: Impervious areas of planning area has to be smaller than totalimpervious area")
+    }
+  }
+  
+  #rainfall <- r2q::get_KOSTRA(duration_string = "0010", plot = T)
+  result <- list()
+  
+  for(i in unique(planning_area$Type)){
+    
+    var <- planning_area %>% dplyr::filter(Type == i)
+    var$area <- var$Wert[var$Einheit == "ha"]
+    
+    var <- var %>% 
+      dplyr::filter(Einheit != "ha") %>% 
+      dplyr::mutate(rain = rainfall$data$Wert[rainfall$data$Jaehrlichkeit == "1 a" &
+                                                rainfall$data$Kategorie == "Regenspende [ l/(s*ha) ]"])
+    
+    # checking that area add to 1
+    if(!var %>% dplyr::summarise(sum(Wert) ==1)){
+      
+      stop(paste("Error: Area ratios of", i, "have to sum to 1"))
+    }
+    
+    
+    var$qe_partial <- var$area*var$Wert*var$fD*var$rain
+    result[[i]] <- var
+  }
+  
+  result <- dplyr::bind_rows(result)
+  
+  
+  QE1_total <- sum(result$qe_partial)
+  QE1_total
+}
+
+
+#' assess_discharges
+#'
+#' @return ratio between tolerable and relevant discharge
+#' @export
+#'
+#' @examples
+#' assess_discharges()
+#' 
+assess_discharges <- function(){    
+  
+  QE1_total <- calculate_relevant_discharge()
+  Q_tolerable_planning <- r2q::calculate_tolerable_discharge(hydrology = r2q::import_hydrology_data(),
+                                                             verbose = F)
+  
+  ratio_QE <- QE1_total / Q_tolerable_planning
+  
+  if(QE1_total > Q_tolerable_planning){
+    print(paste("Discharges exceed tolerable levels by", 
+                round((ratio_QE-1)*100), "%"))
+    print("Consider reducing impervious areas or aquivalent measures ")
+  }
+  
+  if(QE1_total <= Q_tolerable_planning){
+    print(paste("Discharges are below tolerable levels by", 
+                round((ratio_QE-1)*100), "%"))
+  }
+  
+}
+
+
+
+
+
+
+
