06958b042e0ec341e9c198096fc57fe4aabc0193
ma-z-am <43271536+ma-z-am@users.noreply.github.com>
2021-11-02 23:58:29 +0100

    new example scripts

--- R/.load_data.R
+++ R/.load_data.R
@@ -1,4 +1,4 @@
-# load package data
+# load internal package data
 
 c_storm <- r2q::get_stormwater_concentrations()
 c_threshold <- r2q::get_thresholds(LAWA_type = "11")
@@ -6,11 +6,7 @@ local_rain <- r2q::get_KOSTRA(coord_vector = c(3813634.44, 2753912.5),
                               duration_string = 1080, 
                               location_name = "Herne")
 
-library(readxl)
-site_data <- read_excel(path = "inst/extdata/Data_entry/Bsp_Herne.xlsx", 
-                        sheet = "site_data")
-
-# load manually entered data
+# load external data
 siteData <- r2q::load_site_data(
   data.dir = "inst/extdata/Data_entry", 
   filename = "Bsp_Herne.xlsx")
@@ -19,8 +15,19 @@ surfaceData <- r2q::load_surface_data(
   data.dir = "inst/extdata/Data_entry", 
   filename = "Bsp_Herne.xlsx")
 
+c_river <- r2q::load_background_data(
+  data.dir = "inst/extdata/Data_entry", 
+  filename = "Bsp_Herne.xlsx", default_for_na = TRUE)
+
+# combine data
+c_table <- r2q::combine_concentration_tables(
+  threshold_table = c_threshold, 
+  storm_table = c_storm, 
+  background_table = c_river, 
+  onlyComplete = T)
+
 devtools::document()
 
 # erstellen einer Funktion mit Dokumentation
-usethis::use_r("get_package_data")
+usethis::use_r("max_area_and_critical_loads")
 
