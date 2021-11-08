dde10da2bfb0a6d4382863aa9f96abacc8b64dd3
Andreas Matzinger <andreas.matzinger@kompetenz-wasser.de>
2021-11-08 14:52:23 +0100

    ADD adapt load model for r2q substances adds adapted OgRe_model_loads.R, as well as new input and output data

--- R/.OgRe_model_loads.R
+++ R/.OgRe_model_loads.R
@@ -6,7 +6,7 @@ source("R/.abimo_functions_am.R")
 data.dir <- "inst/extdata/OgRe_data/"
 
 #directory for output data
-write.dir <- "data/"
+write.dir <- "inst/extdata/OgRe_data/"
 
 
 ###add separate runoff to output of full 2019 version------------------
@@ -19,7 +19,7 @@ file_name_out <- file.path(data.dir, paste0("abimo_2019_mitstrassen", "out.dbf")
 file_name_in <- file.path(data.dir, paste0("abimo_2019_mitstrassen", ".dbf"))
 
 #read and merge output and input files
-assign("x_comb_SPUR", 
+assign("x_comb_R2Q", 
        abimo_comb_in_out(file_ABIMO_out = file_name_out, file_ABIMO_in = file_name_in))
 
 ##load separate output files
@@ -28,47 +28,50 @@ x_out_streets <- foreign::read.dbf(file.path(data.dir, 'vs_2019_streetsout.dbf')
 x_out_yards <- foreign::read.dbf(file.path(data.dir, 'vs_2019_yardsout.dbf'), as.is = TRUE)
 
 ##add ROW columns
-x_comb_SPUR$ROW_roof <- x_out_roofs$ROW
-x_comb_SPUR$ROW_streets <- x_out_streets$ROW
-x_comb_SPUR$ROW_yards <- x_out_yards$ROW
+x_comb_R2Q$ROW_roof <- x_out_roofs$ROW
+x_comb_R2Q$ROW_streets <- x_out_streets$ROW
+x_comb_R2Q$ROW_yards <- x_out_yards$ROW
 
 
 ##add OgRe Type
 OgRe_Types <- read.table(file = file.path(data.dir, "OgRe_Typen_def.csv"), header = TRUE, sep = ";", dec = ".", as.is = TRUE)
 
-index_typ <- match(x_comb_SPUR$TYP, OgRe_Types$Flaechentyp)
+index_typ <- match(x_comb_R2Q$TYP, OgRe_Types$Flaechentyp)
 
-x_comb_SPUR$OgRe_Type <- OgRe_Types$OgRe_Typ_klar[index_typ]
+x_comb_R2Q$OgRe_Type <- OgRe_Types$OgRe_Typ_klar[index_typ]
 
 ##calculate loads by BTF
 
-#find OgRe-Model concentrations for SPUR substances
+#find OgRe-Model concentrations for R2Q substances
+##get concentration thresholds for SUW_type
+R2Q_subs <- read.table(file = file.path(data.dir, "r2q_subs.csv"), header = TRUE, sep = ";", dec = ".")
+
 OgRe_conc <- read.table(file = file.path(data.dir, "annual_mean_conc.csv"), 
                         header = TRUE, sep = ";", dec = ".", as.is = TRUE)
 
-SPUR_subs <- kwb.ogre::OGRE_VARIABLES()[c(18,26,48,55,57,65,66,67),]
+index <- match(R2Q_subs$Variable_name, OgRe_conc$VariableName)
 
-index <- match(SPUR_subs$VariableName, OgRe_conc$VariableName)
+OgRe_conc_R2Q <- OgRe_conc[index,]
 
-OgRe_conc_SPUR <- OgRe_conc[index,]
+OgRe_conc_R2Q$VariableAbbrev <- R2Q_subs$abbrev
 
-names(OgRe_conc_SPUR)[9] <- "AND"
+names(OgRe_conc_R2Q)[9] <- "AND"
 
 #calculate load for each BTF in kg/yr
-index <- match(x_comb_SPUR$OgRe_Type, names(OgRe_conc_SPUR))
+index <- match(x_comb_R2Q$OgRe_Type, names(OgRe_conc_R2Q))
 
-for (subs in OgRe_conc_SPUR$VariableName) {
+for (subs in OgRe_conc_R2Q$VariableAbbrev) {
   
-  x_comb_SPUR[[paste0(subs,"_kg_yr")]] <- x_comb_SPUR$ROW/1000 * #runoff in m
-                                          x_comb_SPUR$FLAECHE *  #area in m2
-                                          as.vector(t(OgRe_conc_SPUR[which(OgRe_conc_SPUR$VariableName == subs), index])) / 1e6 #concentration in kg/m3
+  x_comb_R2Q[[paste0(subs,"_kg_yr")]] <- x_comb_R2Q$ROW/1000 * #runoff in m
+                                          x_comb_R2Q$FLAECHE *  #area in m2
+                                          as.vector(t(OgRe_conc_R2Q[which(OgRe_conc_R2Q$VariableAbbrev == subs), index])) / 1e6 #concentration in kg/m3
   
 }
 
 ##order and write combined file to match map
 
-x_comb_SPUR_map <- ABIMO_adapt_map(ABIMO_out = x_comb_SPUR, 
-                out_file = file.path(data.dir, 'vs_2019_SPUR.dbf'),
+x_comb_R2Q_map <- ABIMO_adapt_map(ABIMO_out = x_comb_R2Q, 
+                out_file = file.path(data.dir, 'vs_2019_R2Q.dbf'),
                 file_georef = file.path(data.dir, "ISU5_ID.dbf") ) 
 
 
