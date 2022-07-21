e1d628da479d2032fc61ebd542330d7d8ce44c69
ma-z-am <malte.zamzow@kompetenz-wasser.de>
2022-07-21 19:12:56 +0200

    Add color palette as data in Package with documentation

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Color palette for R2Q Plots
+#' 
+#' Contains 3 different colors "orange", "blue" and "green" in 6 different 
+#' shades
+#' 
+#' @format Data frame with 3 columns for the colors and 6 rows for the shades
+#' @source Logo of the R2Q project
+#' 
+"r2q_pal"
