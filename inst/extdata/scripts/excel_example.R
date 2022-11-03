39f0c531c08f380e20611352bc7d94dc07dabc52
ma-z-am <malte.zamzow@kompetenz-wasser.de>
2022-11-03 18:15:15 +0100

    Add r2q Package name to functions

--- inst/extdata/scripts/excel_example.R
+++ inst/extdata/scripts/excel_example.R
@@ -1,10 +1,8 @@
-
-
-status_quo <- run_status_quo(
+status_quo <- r2q::run_status_quo(
   path = "inst/extdata/Data_entry", 
   filename = "Baukau_final.xlsx", 
   c_type = "average")
 
-scenario <- run_scenario(
+scenario <- r2q::run_scenario(
   status_quo_list = status_quo, 
   scenario_name = "planning_area_details")
